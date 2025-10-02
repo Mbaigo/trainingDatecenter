166922a1d567823027df19215ec8bab56ef8d022
Mbaigo <mbaigolmemevrard@gmail.com>
2025-10-02 22:15:07 +0400

    Add controllerAdvice

---
+++ src/main/java/com/mbaigo/trainingTools/training_tools/exception/TrainingExceptionHandler.java
@@ -0,0 +1,36 @@
+package com.mbaigo.trainingTools.training_tools.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+import java.time.LocalDateTime;
+
+@ControllerAdvice
+public class TrainingExceptionHandler {
+
+    @ExceptionHandler(TrainingApiException.class)
+    public ResponseEntity<TrainingApiExceptionResponse> handleTrainingApiException(TrainingApiException ex, WebRequest request) {
+        TrainingApiExceptionResponse response = new TrainingApiExceptionResponse(
+                LocalDateTime.now(),
+                ex.getStatus(),
+                ex.getMessage(),
+                ex.getDetails() != null ? ex.getDetails() : request.getDescription(false)
+        );
+        return new ResponseEntity<>(response, HttpStatus.valueOf(ex.getStatus()));
+    }
+
+    // Gestion d'autres exceptions globales si besoin
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<TrainingApiExceptionResponse> handleAllExceptions(Exception ex, WebRequest request) {
+        TrainingApiExceptionResponse response = new TrainingApiExceptionResponse(
+                LocalDateTime.now(),
+                HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                ex.getMessage(),
+                request.getDescription(false)
+        );
+        return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
